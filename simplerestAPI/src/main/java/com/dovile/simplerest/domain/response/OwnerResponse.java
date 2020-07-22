965efc9aae1157bca16adc4b3066eff3aa51535b
Doreina <barkauskaite.dovile@gmail.com>
2020-07-22 22:47:41 +0300

    create OwnerResponse class which return Owner without any ID

---
+++ simplerestAPI/src/main/java/com/dovile/simplerest/domain/response/OwnerResponse.java
@@ -0,0 +1,21 @@
+package com.dovile.simplerest.domain.response;
+
+public class OwnerResponse {
+
+    private String name;
+
+    public OwnerResponse() {
+
+    }
+    public OwnerResponse(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
