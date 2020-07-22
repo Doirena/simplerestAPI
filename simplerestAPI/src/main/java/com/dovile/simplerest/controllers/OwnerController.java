659e2fdc3b24bfc88b29aea7c5ca430c4a0343d2
Doreina <barkauskaite.dovile@gmail.com>
2020-07-22 23:24:49 +0300

    add getmapping owner by id

--- simplerestAPI/src/main/java/com/dovile/simplerest/controllers/OwnerController.java
+++ simplerestAPI/src/main/java/com/dovile/simplerest/controllers/OwnerController.java
@@ -3,8 +3,10 @@ package com.dovile.simplerest.controllers;
 import com.dovile.simplerest.domain.request.OwnerRequest;
 import com.dovile.simplerest.domain.response.OwnerResponse;
 import com.dovile.simplerest.entities.OwnerEntity;
+import com.dovile.simplerest.exception.ResourceNotFoundException;
 import com.dovile.simplerest.services.OwnerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -18,12 +20,18 @@ public class OwnerController {
     private OwnerService ownerService;
 
     @GetMapping
-    public List<OwnerResponse> getAllOwners(){
+    public List<OwnerResponse> getAllOwners() {
         return ownerService.findAllOwners();
     }
 
+    @GetMapping("/{id}")
+    public OwnerResponse getOwnerById(@PathVariable(value = "id") Integer ownerId)
+            throws ResourceNotFoundException {
+        return ownerService.findOwnerById(ownerId);
+    }
+
     @PostMapping("/addOwner")
-    public OwnerResponse createOwner(@RequestBody @Valid OwnerRequest owner){
+    public OwnerResponse createOwner(@RequestBody @Valid OwnerRequest owner) {
         return ownerService.createOwner(owner);
     }
 
