0ae351c01aa1e405d0fa5fd68d38f9bc39880ef4
Doreina <barkauskaite.dovile@gmail.com>
2020-07-22 22:02:05 +0300

    create Owner domain class

---
+++ simplerestAPI/src/main/java/com/dovile/simplerest/domain/Owner.java
@@ -0,0 +1,14 @@
+package com.dovile.simplerest.domain;
+
+public class Owner {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
