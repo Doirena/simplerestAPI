7176ee6e58c108cd8d96ef3fbcad3ef5ff19051b
Doreina <barkauskaite.dovile@gmail.com>
2020-07-22 22:07:31 +0300

    delete

--- simplerestAPI/src/main/java/com/dovile/simplerest/repositories/OwnerEntityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dovile.simplerest.repositories;
-
-import com.dovile.simplerest.entities.OwnerEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OwnerEntityRepository extends JpaRepository<OwnerEntity, Integer> {
-}
