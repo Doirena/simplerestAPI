fea83759f4c5768af828618518c6dd429b5badad
Doreina <barkauskaite.dovile@gmail.com>
2020-07-23 00:28:24 +0300

    Correct name: it was PropertyRespository

--- simplerestAPI/src/main/java/com/dovile/simplerest/repositories/PropertyRespository.java
+++ simplerestAPI/src/main/java/com/dovile/simplerest/repositories/PropertyRespository.java
@@ -1,11 +1,11 @@
-package com.dovile.simplerest.domain.response;
+package com.dovile.simplerest.repositories;
 
 import com.dovile.simplerest.entities.PropertyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface PropertyResponse extends JpaRepository<PropertyEntity, Integer> {
+public interface PropertyRespository extends JpaRepository<PropertyEntity, Integer> {
     @Query(name = "PropertyEntity.findBYType")
     PropertyEntity findBYType(@Param("property") String property);
 }
