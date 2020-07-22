ba661067de5fbdf8431480709b0ec2dce0a46c62
Doreina <barkauskaite.dovile@gmail.com>
2020-07-22 23:23:32 +0300

    method get Owner by ID

--- simplerestAPI/src/main/java/com/dovile/simplerest/services/OwnerServiceImpl.java
+++ simplerestAPI/src/main/java/com/dovile/simplerest/services/OwnerServiceImpl.java
@@ -3,8 +3,10 @@ package com.dovile.simplerest.services;
 import com.dovile.simplerest.domain.request.OwnerRequest;
 import com.dovile.simplerest.domain.response.OwnerResponse;
 import com.dovile.simplerest.entities.OwnerEntity;
+import com.dovile.simplerest.exception.ResourceNotFoundException;
 import com.dovile.simplerest.repositories.OwnerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -17,30 +19,32 @@ public class OwnerServiceImpl implements OwnerService {
     @Autowired
     private OwnerRepository ownerRepository;
 
-
     public List<OwnerResponse> findAllOwners() {
-       List<OwnerResponse>ownerList = new ArrayList<OwnerResponse>();
-       List<OwnerEntity> ownerEList = ownerRepository.findAll();
-       for (int i=0; i<ownerEList.size(); i++){
-           ownerList.add(new OwnerResponse(ownerEList.get(i).getName()));
-       }
+        List<OwnerResponse> ownerList = new ArrayList<OwnerResponse>();
+        List<OwnerEntity> ownerEList = ownerRepository.findAll();
+        for (int i = 0; i < ownerEList.size(); i++) {
+            ownerList.add(new OwnerResponse(ownerEList.get(i).getName()));
+        }
         return ownerList;
     }
 
-    public Optional<OwnerEntity> findOwnerById(Integer id) {
-        return Optional.empty();
+    public OwnerResponse findOwnerById(Integer id) throws ResourceNotFoundException {
+        OwnerResponse ownerResponse = new OwnerResponse();
+        OwnerEntity ownerE = ownerRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Owner not found on: " + id));
+        ownerResponse.setName(ResponseEntity.ok().body(ownerE).getBody().getName());
+        return ownerResponse;
     }
 
 
     public OwnerResponse createOwner(OwnerRequest owner) {
-        OwnerEntity ownerE =new OwnerEntity();
+        OwnerEntity ownerE = new OwnerEntity();
         ownerE.setName(owner.getName());
         ownerRepository.save(ownerE);
-        OwnerResponse ownerResponse=new OwnerResponse();
+        OwnerResponse ownerResponse = new OwnerResponse();
         ownerResponse.setName(ownerE.getName());
         return ownerResponse;
     }
-
 
     public void deleteOwnerById(Integer id) {
 
