2abc1b6f02e04986e06b20dbb8747bcfa96f656b
Doreina <barkauskaite.dovile@gmail.com>
2020-07-23 00:22:56 +0300

    PropertyEntity add parameters

--- simplerestAPI/src/main/java/com/dovile/simplerest/entities/PropertyEntity.java
+++ simplerestAPI/src/main/java/com/dovile/simplerest/entities/PropertyEntity.java
@@ -1,9 +1,38 @@
 package com.dovile.simplerest.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "property")
 public class PropertyEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String type;
+    private double tax_rate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getTax_rate() {
+        return tax_rate;
+    }
+
+    public void setTax_rate(double tax_rate) {
+        this.tax_rate = tax_rate;
+    }
 }
