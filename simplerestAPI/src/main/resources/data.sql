f9c5f7dcb95a670c57e465311b25f16ce4ddb1d6
Doreina <barkauskaite.dovile@gmail.com>
2020-07-23 14:44:21 +0300

    add data of buildrecord

--- simplerestAPI/src/main/resources/data.sql
+++ simplerestAPI/src/main/resources/data.sql
@@ -7,3 +7,11 @@ INSERT INTO property (type, tax_rate ) VALUES ('Flat', 20.5);
 INSERT INTO property (type, tax_rate ) VALUES ('Garden House', 50);
 INSERT INTO property (type, tax_rate ) VALUES ('Apartment', 65.20);
 INSERT INTO property (type, tax_rate ) VALUES ('House', 75.75);
+
+INSERT INTO building_records(address, owner_id, size, value, property_type_id) VALUES('Naugardukas str. 1, Vilnius', 1, 38, 20, 1);
+INSERT INTO building_records(address, owner_id, size, value, property_type_id) VALUES('Vilnius str. 11, Vilnius', 1, 200, 2.2, 4);
+INSERT INTO building_records(address, owner_id, size, value, property_type_id) VALUES('Vilnius str. 21, Vilnius', 2, 21, 3.2, 3);
+INSERT INTO building_records(address, owner_id, size, value, property_type_id) VALUES('Vilnius str. 41, Vilnius', 3, 100, 5.2, 2);
+INSERT INTO building_records(address, owner_id, size, value, property_type_id) VALUES('Birute str. 21, Vilnius', 4, 78, 6.2, 1);
+INSERT INTO building_records(address, owner_id, size, value, property_type_id) VALUES('Vytauto str. 51, Vilnius', 1, 178, 4.2, 2);
+INSERT INTO building_records(address, owner_id, size, value, property_type_id) VALUES('Vytauto str. 51, Vilnius', 3, 100, 10, 1);
