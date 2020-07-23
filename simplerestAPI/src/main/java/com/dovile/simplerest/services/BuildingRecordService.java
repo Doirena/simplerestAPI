931f2337df5dd05a0fa35747f4ea1ba5fdf40e9c
Doreina <barkauskaite.dovile@gmail.com>
2020-07-23 16:45:16 +0300

    add new method createRecord

--- simplerestAPI/src/main/java/com/dovile/simplerest/services/BuildingRecordService.java
+++ simplerestAPI/src/main/java/com/dovile/simplerest/services/BuildingRecordService.java
@@ -1,5 +1,6 @@
 package com.dovile.simplerest.services;
 
+import com.dovile.simplerest.domain.request.BuildingRecordsRequest;
 import com.dovile.simplerest.domain.response.BuildingRecordResponse;
 import com.dovile.simplerest.entities.BuildingRecordEntity;
 import org.springframework.stereotype.Service;
@@ -10,10 +11,20 @@ import java.util.List;
  * @author barkauskaite.dovile@gmail.com
  */
 public interface BuildingRecordService {
+
     /**
      *
      * @return list of building records
      */
     List<BuildingRecordResponse> findAllRecords();
+
+    /**
+     *
+     * @param record
+     * @param owner
+     * @param property
+     * @return new Building Record
+     */
+    BuildingRecordResponse createRecord(BuildingRecordsRequest record, String owner, String property);
 
 }
