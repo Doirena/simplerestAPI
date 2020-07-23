e113a0ba6b2133bfbad479ee82c5573709fea39c
Doreina <barkauskaite.dovile@gmail.com>
2020-07-23 15:27:05 +0300

    create class BuildingRecordResponse and add all parameters

---
+++ simplerestAPI/src/main/java/com/dovile/simplerest/domain/response/BuildingRecordResponse.java
@@ -0,0 +1,61 @@
+package com.dovile.simplerest.domain.response;
+
+public class BuildingRecordResponse {
+    private String address;
+    private double size;
+    private double value;
+    private OwnerResponse owner;
+    private PropertyResponse property;
+
+    public BuildingRecordResponse() {
+    }
+
+    public BuildingRecordResponse(String address, double size, double value,
+                                  OwnerResponse owner, PropertyResponse property) {
+        this.address = address;
+        this.size = size;
+        this.value = value;
+        this.owner = owner;
+        this.property = property;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public OwnerResponse getOwner() {
+        return owner;
+    }
+
+    public void setOwner(OwnerResponse owner) {
+        this.owner = owner;
+    }
+
+    public PropertyResponse getProperty() {
+        return property;
+    }
+
+    public void setProperty(PropertyResponse property) {
+        this.property = property;
+    }
+}
