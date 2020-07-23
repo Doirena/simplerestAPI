39f14192010148c6cef122b7e4279c0c6de0e4bd
Doreina <barkauskaite.dovile@gmail.com>
2020-07-23 10:56:33 +0300

    add property data in to data.sql

--- simplerestAPI/src/main/resources/data.sql
+++ simplerestAPI/src/main/resources/data.sql
@@ -2,3 +2,8 @@ INSERT INTO owner (name) VALUES ('Anna');
 INSERT INTO owner (name) VALUES ('Ben');
 INSERT INTO owner (name) VALUES ('Victoria');
 INSERT INTO owner (name) VALUES ('John');
+
+INSERT INTO property (type, tax_rate ) VALUES ('Flat', 20.5);
+INSERT INTO property (type, tax_rate ) VALUES ('Garden House', 50);
+INSERT INTO property (type, tax_rate ) VALUES ('Apartment', 65.20);
+INSERT INTO property (type, tax_rate ) VALUES ('House', 75.75);
