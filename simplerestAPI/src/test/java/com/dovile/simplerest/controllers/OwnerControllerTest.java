1cad88b4775b650c7a8ad6f0b993fd09d0c45518
Doreina <barkauskaite.dovile@gmail.com>
2020-07-23 23:20:02 +0300

    add 3 tests-> createOwner_test and updateOwner_test, deleteOwner_test

--- simplerestAPI/src/test/java/com/dovile/simplerest/controllers/OwnerControllerTest.java
+++ simplerestAPI/src/test/java/com/dovile/simplerest/controllers/OwnerControllerTest.java
@@ -1,5 +1,6 @@
 package com.dovile.simplerest.controllers;
 
+import com.dovile.simplerest.domain.request.OwnerRequest;
 import com.dovile.simplerest.domain.response.OwnerResponse;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,11 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.HttpClientErrorException;
 
 import static org.junit.Assert.*;
 
@@ -46,14 +45,37 @@ public class OwnerControllerTest {
     }
 
     @Test
-    public void createOwner() {
+    public void createOwner_test() {
+        OwnerRequest owner = new OwnerRequest();
+        owner.setName("Dovile");
+        ResponseEntity<OwnerResponse> postResponse = restTemplate.postForEntity(getRootUrl() + "/owner", owner,
+                OwnerResponse.class);
+        assertNotNull(postResponse);
+        assertNotNull(postResponse.getBody());
     }
 
     @Test
     public void updateOwner() {
+        Integer id = 1;
+        OwnerResponse ownerResponse = restTemplate.getForObject(getRootUrl() + "/" + id, OwnerResponse.class);
+        OwnerRequest owner = new OwnerRequest();
+        owner.setName("Dovile");
+        restTemplate.put(getRootUrl() + "/" + id, owner);
+        OwnerResponse updatedOwner = restTemplate.getForObject(getRootUrl() +"/" + id, OwnerResponse.class);
+        assertEquals(owner.getName(),updatedOwner.getName());
+        assertNotNull(updatedOwner);
     }
 
     @Test
-    public void deleteOwner() {
+    public void deleteOwner_test() {
+        Integer id = 2;
+        OwnerResponse owner = restTemplate.getForObject(getRootUrl() + "/" + id, OwnerResponse.class);
+        assertNotNull(owner);
+        restTemplate.delete(getRootUrl() + "/" + id);
+        try {
+            owner = restTemplate.getForObject(getRootUrl() + "/" + id, OwnerResponse.class);
+        } catch (final HttpClientErrorException e) {
+            assertEquals(e.getStatusCode(), HttpStatus.NOT_FOUND);
+        }
     }
 }
