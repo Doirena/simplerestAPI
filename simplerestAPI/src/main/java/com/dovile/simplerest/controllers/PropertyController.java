b131a964a44e1b5292e0343901e3bb743e626cbc
Doreina <barkauskaite.dovile@gmail.com>
2020-07-23 12:07:02 +0300

    add GetMapping property get by id

--- simplerestAPI/src/main/java/com/dovile/simplerest/controllers/PropertyController.java
+++ simplerestAPI/src/main/java/com/dovile/simplerest/controllers/PropertyController.java
@@ -1,9 +1,11 @@
 package com.dovile.simplerest.controllers;
 
 import com.dovile.simplerest.domain.response.PropertyResponse;
+import com.dovile.simplerest.exception.ResourceNotFoundException;
 import com.dovile.simplerest.services.PropertyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +21,11 @@ public class PropertyController {
     @GetMapping
     public List<PropertyResponse> getAllProperties(){
         return propertyService.findAllProperties();
+    }
+
+    @GetMapping("/{id}")
+    public PropertyResponse getPropertyByID(@PathVariable (value = "id") Integer propertyId)
+            throws ResourceNotFoundException {
+        return propertyService.findPropertyById(propertyId);
     }
 }
