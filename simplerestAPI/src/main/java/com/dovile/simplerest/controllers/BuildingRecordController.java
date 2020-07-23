95ad66d41a35ce3ca110de80c99d97c4743b5a0c
Doreina <barkauskaite.dovile@gmail.com>
2020-07-23 18:42:08 +0300

    add PutMapping which update BuildingRecord

--- simplerestAPI/src/main/java/com/dovile/simplerest/controllers/BuildingRecordController.java
+++ simplerestAPI/src/main/java/com/dovile/simplerest/controllers/BuildingRecordController.java
@@ -2,10 +2,12 @@ package com.dovile.simplerest.controllers;
 
 import com.dovile.simplerest.domain.request.BuildingRecordsRequest;
 import com.dovile.simplerest.domain.response.BuildingRecordResponse;
+import com.dovile.simplerest.exception.ResourceNotFoundException;
 import com.dovile.simplerest.services.BuildingRecordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -15,15 +17,22 @@ public class BuildingRecordController {
     private BuildingRecordService buildingRecordService;
 
     @GetMapping
-    public List<BuildingRecordResponse> findAllRecords(){
+    public List<BuildingRecordResponse> findAllRecords() {
         return buildingRecordService.findAllRecords();
     }
 
-    @PostMapping(value ="/record",  params = {"owner","property"})
+    @PostMapping(value = "/record", params = {"owner", "property"})
     public BuildingRecordResponse createRecord(
             @RequestBody BuildingRecordsRequest record,
             @RequestParam(value = "owner") String owner,
-            @RequestParam(value = "property") String property){
+            @RequestParam(value = "property") String property) {
         return buildingRecordService.createRecord(record, owner, property);
+    }
+
+    @PutMapping("/{id}")
+    public BuildingRecordResponse updateRecord(@PathVariable(value = "id") Integer recordId,
+                                               @RequestBody @Valid BuildingRecordsRequest record)
+            throws ResourceNotFoundException {
+        return buildingRecordService.refurbishRecord(recordId, record);
     }
 }
