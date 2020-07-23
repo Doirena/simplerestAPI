8eeb221580d80125b2cc4168b65122953205bd4f
Doreina <barkauskaite.dovile@gmail.com>
2020-07-23 11:07:59 +0300

    create PropertyRequest class with all parameters

---
+++ simplerestAPI/src/main/java/com/dovile/simplerest/domain/request/PropertyRequest.java
@@ -0,0 +1,30 @@
+package com.dovile.simplerest.domain.request;
+
+public class PropertyRequest {
+    private String type;
+    private double tax_rate;
+
+    public PropertyRequest() {
+    }
+
+    public PropertyRequest(String type, double tax_rate) {
+        this.type = type;
+        this.tax_rate = tax_rate;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getTax_rate() {
+        return tax_rate;
+    }
+
+    public void setTax_rate(double tax_rate) {
+        this.tax_rate = tax_rate;
+    }
+}
