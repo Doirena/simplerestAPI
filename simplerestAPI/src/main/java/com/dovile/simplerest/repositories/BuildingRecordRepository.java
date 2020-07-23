c1a1cba612060c3cd5505dd0ceff7092f14313b3
Doreina <barkauskaite.dovile@gmail.com>
2020-07-23 19:26:22 +0300

    add query into repository

--- simplerestAPI/src/main/java/com/dovile/simplerest/repositories/BuildingRecordRepository.java
+++ simplerestAPI/src/main/java/com/dovile/simplerest/repositories/BuildingRecordRepository.java
@@ -2,8 +2,12 @@ package com.dovile.simplerest.repositories;
 
 import com.dovile.simplerest.entities.BuildingRecordEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BuildingRecordRepository extends JpaRepository<BuildingRecordEntity,Integer> {
+    @Query(name = "BuildingRecordEntity.RealEstateTaxes")
+    String RealEstateTaxes (@Param("id") Integer id);
 }
